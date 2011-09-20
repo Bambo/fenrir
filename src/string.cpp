5ab4d1649748f725d97f72a392fb9e2bab4c1406
Mikael Lindberg <aualin@gmail.com>
2011-09-20 11:15:14 +0200

    Fix string class

--- src/string.cpp
+++ src/string.cpp
@@ -6,32 +6,25 @@
 #include <Debug.hpp>
 namespace fenrir {
 	string::string(const char* str) {
-		if(mString == NULL) {
-			DBGPRT("is null ")
-		} else {
-			DBGPRT("not null")
-		}
 		int len = std::strlen(str);
-		mString = (char*)malloc(sizeof(char)*len+1);
+		mString = new char[len+1];
 		std::memcpy(mString, str, len);
 		mString[len] = '\0';
-		DBGPRT("String is now " << mString)
 	}
 
 	string& string::operator+=(const string& rh) {
 		int len = std::strlen(mString)+strlen(rh.mString);
-		char* tmpStr = (char*)std::malloc(len-1); // One less zero since we combine ;)
+		char* tmpStr = new char[len+1]; 
+		//std::memset(tmpStr, '\0', len); // Valgrind goes mad if i don't do this, needs more investigation
 		std::memcpy(tmpStr, mString, std::strlen(mString));
 		std::strcat(tmpStr, rh.mString);
-		tmpStr[len-1] = '\0';
-		free(mString);
+		delete[] mString;
 		mString = tmpStr;
-		DBGPRT("String is naow " << mString)
+		mString[len] = '\0';
 		return *this;
 	}
 
 	string::~string() {
-		DBGPRT("NOW DELETING " << mString)
-		free(mString);
+		delete[] mString;
 	}
 }
