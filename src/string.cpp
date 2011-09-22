b9d764123e8821668345be9356488d95fe222649
Mikael Lindberg <aualin@gmail.com>
2011-09-22 23:01:20 +0200

    Fix string crash on Windows

--- src/string.cpp
+++ src/string.cpp
@@ -5,17 +5,18 @@
 #include <iostream>
 #include <Debug.hpp>
 namespace fenrir {
+
 	string::string(const char* str) {
 		int len = std::strlen(str);
 		mString = new char[len+1];
-		std::memcpy(mString, str, len);
-		mString[len] = '\0';
+		std::memcpy(mString, str, len+1);
 	}
 
 	string& string::operator+=(const string& rh) {
+		DBGPRT(mString << " :::::::: " << rh.mString << "\n")
 		int len = std::strlen(mString)+strlen(rh.mString);
 		char* tmpStr = new char[len+1]; 
-		//std::memset(tmpStr, '\0', len); // Valgrind goes mad if i don't do this, needs more investigation
+		std::memset(tmpStr, '\0', len); // Valgrind goes mad if i don't do this, needs more investigation
 		std::memcpy(tmpStr, mString, std::strlen(mString));
 		std::strcat(tmpStr, rh.mString);
 		delete[] mString;
